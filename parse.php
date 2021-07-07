f51371700dc9ee17f1a5a4962f91eac4558471d5
Aliaksei <mozh.by@gmail.com>
2021-07-07 09:44:07 +0300

    Change field

--- parse.php
+++ parse.php
@@ -2,7 +2,7 @@
 
 $file_name = 'PH_content_export_elearn_article_2021-06-15.csv';
 $path_to_file = 'content/' . $file_name;
-$field_image_name = 'field_image';
+$field_body_name = 'body_value';
 
 $title_field = 'title_field';
 $field_image_alt_name = 'field_image_alt';
@@ -10,7 +10,7 @@ $field_image_alt_name = 'field_image_alt';
 $new_path = '/sites/default/files/migration_images/';
 
 $field_title_key = NULL;
-$field_image_key = NULL;
+$field_body_key = NULL;
 $field_image_alt_key = NULL;
 
 //Read csv in array
@@ -20,38 +20,45 @@ if (($handle = fopen($path_to_file, 'r')) !== FALSE) {
     $csv[] = $data;
   }
 
-  //Try to find key for column with $field_image_name
+  //Try to find key for column with $field_body_name
   $field_key = NULL;
   if (!empty($csv[0])) {
     $field_title_key = array_search($title_field, $csv[0]);
-    $field_image_key = array_search($field_image_name, $csv[0]);
+    $field_body_key = array_search($field_body_name, $csv[0]);
     $field_image_alt_key = array_search($field_image_alt_name, $csv[0]);
   }
   else {
     print '<h2 style="color: red">Empty headers</h2>';
   }
 
-  //Get all values from column $field_image_name
+  //Get all values from column $field_body_name
   $field_values = [];
-  if (!empty($field_image_key)) {
+  if (!empty($field_body_key)) {
+    $re = '!(https?:)?//\S+\.(?:jpe?g|jpg|png|gif)!Ui';
     print '<h2 style="color: green">Count rows '.count($csv).'</h2>';
     print '<table border="1"><tr><th>Old path</th><th>New Path</th></tr>';
     foreach ($csv as $key => &$c) {
       if ($key == 0) {//don't get header
         continue;
       }
-      if (!empty($c[$field_image_key])) {
-        $value = $c[$field_image_key];
-        $arr_srt = explode('/', $value);
-        $new_str = $new_path . $arr_srt[count($arr_srt) - 1];
-        $c[$field_image_key] = $new_str;
-        print '<tr><td>' . $value . '</td><td>' . $new_str . '</td></tr>';
+      if (!empty($c[$field_body_key])) {
+        $value = $c[$field_body_key];
+        preg_match_all($re, $value, $matches);
+        if (!empty($matches[0])) {
+          foreach ($matches[0] as $match) {
+            $arr_srt = explode('/', $match);
+            $new_str = '/sites/default/files/migration_images/' . $arr_srt[count($arr_srt) - 1];
+            $value = str_replace($match, $new_str, $value);
+            $c[$field_body_key] = $value;
+            print '<tr><td>' . $match . '</td><td>' . $new_str . '</td></tr>';
+          }
+        }
       }
     }
     print '</table>';
   }
   else {
-    print '<h2 style="color: red">Can\'t find key for your field:'.$field_image_name.'</h2>';
+    print '<h2 style="color: red">Can\'t find key for your field:'.$field_body_name.'</h2>';
   }
 
   //Fill alt for image from title
